7ea0a6df151f9c8552529de0590bbcd3625eb907
Ashton Reimer <ashton.reimer@sri.com>
2019-01-30 12:13:14 -0800

    Fixed some issues with setup.py. It's working again.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013  Ashton S. Reimer
+# Copyright (C) 2019  Ashton S. Reimer
 # Full license can be found in LICENSE.txt
 import os
-from setuptools import find_packages, setup
 import subprocess
+from setuptools import find_packages, setup
 
-requirements = ['pathlib2','cartopy','numpy','matplotlib','h5py']
+requirements = ['pathlib2','cartopy','numpy','matplotlib','h5py','cython']
 
 try:
     subprocess.call(['conda','install',' '.join(requirements)])
@@ -14,9 +14,11 @@ except Exception:
     pass
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).readlines()
-
+readme = os.path.join(os.path.dirname(__file__), 'README.md')
+with open(readme,'r') as f:
+    readme_txt = f.readlines()
+readme_txt = '\n'.join(readme_txt)
+    
 
 #############################################################################
 # First, check to make sure we are executing
@@ -34,6 +36,7 @@ repo root directory."
 #############################################################################
 setup(name='pyAMISR',
       install_requires=requirements,
+      setup_requires=requirements,
       version="1.1",
       description="A library of data plotting utilities for visualizing processed Advanced Modular Incoherent Scatter Radar (AMISR) data.",
       author="Ashton S. Reimer",
@@ -41,7 +44,7 @@ setup(name='pyAMISR',
       url="https://github.com/asreimer/pyAMISR",
       download_url="https://github.com/asreimer/pyAMISR",
       packages=find_packages(),
-      long_description=read('README.rst'),
+      long_description=readme_txt,
       zip_safe=False,
       py_modules=['pyAMISR'],
       classifiers=[
